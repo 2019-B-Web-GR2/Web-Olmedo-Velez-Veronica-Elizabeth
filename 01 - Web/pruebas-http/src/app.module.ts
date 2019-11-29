6b4b163220f248d16b778d29af6b58ba94c43c57
Nika25 <veronica.olmedo@epn.edu.ec>
2019-11-29 16:15:20 -0500

    Base de Datos

--- 01 - Web/pruebas-http/src/app.module.ts
+++ 01 - Web/pruebas-http/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import {TypeOrmCoreModule} from "@nestjs/typeorm/dist/typeorm-core.module";
+import {UsuarioEntity} from "./usuario/usuario.entity";
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmCoreModule.forRoot(
+        {
+          type: 'mysql',
+          host: 'localhost',
+          port: 32775,
+          username: 'nika',
+          password: '1234',
+          database: 'web',
+          entities: [
+              UsuarioEntity
+          ],
+          synchronize: true,
+
+        })
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
