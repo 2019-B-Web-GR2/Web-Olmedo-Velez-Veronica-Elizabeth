91b77823614bea6cd3ee4b89fef2f061ba4259d7
Nika25 <veronica.olmedo@epn.edu.ec>
2019-12-13 16:33:09 -0500

    Usuarios

--- 01 - Web/pruebas-http/src/usuario/usuario.controller.ts
+++ 01 - Web/pruebas-http/src/usuario/usuario.controller.ts
@@ -1,7 +1,39 @@
-import { Controller} from '@nestjs/common';
+import {Body, Controller, Get, Param, Post} from "@nestjs/common";
+import {UsuarioService} from "./usuario.service";
+import {UsuarioEntity} from "./usuario.entity";
 
 @Controller('usuario')
-export class UsuarioController{
+export class UsuarioController {
+    constructor(
+        private readonly _usuarioService: UsuarioService,
+    ) {
 
+    }
+
+
+    // GET /modelo/:id
+    @Get(':id')
+    obtenerUnUsuario(
+        @Param('id') identificador: string,
+    ): Promise<UsuarioEntity | undefined> {
+        return this._usuarioService
+            .encontrarUno(
+                Number(identificador)
+            );
+    }
+
+    @Post()
+    crearUnUsuario(
+        @Body() usuario: UsuarioEntity,
+    ): Promise<UsuarioEntity> {
+        return this._usuarioService
+            .crearUno(
+                usuario
+            );
+    }
+
+    @Get('hola')
+    hola(): string {
+        return 'Hola';
+    }
 }
-
